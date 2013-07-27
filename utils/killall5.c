b490d93a1ad59f7a52d8f64a14d157b7741b2c61
Subsentient <thinkingrodent@gmail.com>
2013-07-27 06:43:32 -0700

    Rewrite a lot of killall5.c to prevent killing our own shell and scripts running in that shell.

--- utils/killall5.c
+++ utils/killall5.c
@@ -2,18 +2,28 @@
 * This software is public domain.
 * Please read the file LICENSE.TXT for more information.*/
 
-/**Version 0.1**/
+/**Version 0.2**/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <ctype.h>
+#include <dirent.h>
 #include "../mauri.h"
 
 int main(int argc, char **argv)
 {
 	unsigned char InSignal;
 	char *InArg;
+	DIR *ProcDir;
+	struct dirent *CurDir;
+	unsigned long CurPID, OurPID, Inc;
+	FILE *TempDescriptor;
+	char TmpBuf[1024], SessionID[8192], SessionID_Targ[8192], TChar;
+	
 	
 	if (argc > 2) /*Hmm, looks like we are being passed more than one argument.*/
 	{
@@ -44,6 +54,69 @@ int main(int argc, char **argv)
 				"Bad argument provided. Please enter a valid signal number.\n");
 		}
 	}
+
+	OurPID = getpid(); /*We need this so we don't kill ourselves.*/
 	
-	return kill(-1, InSignal); /*Well, since kill() returns non-zero on failure, I see no problem here.*/
+	/*Get our Session ID in ASCII, because it's often too big to fit in a 32 bit integer.
+	 * Anything in our session ID must live so that our shell lives.*/
+	snprintf(TmpBuf, 1024, "/proc/%lu/sessionid", OurPID);
+	
+	if (!(TempDescriptor = fopen(TmpBuf, "r")))
+	{
+		fprintf(stderr, "Failed to read session ID file for ourselves. Aborting.");
+		return 1;
+	}
+	
+	for (Inc = 0; (TChar = getc(TempDescriptor)) != EOF; ++Inc)
+	{
+		SessionID[Inc] = TChar;
+	}
+	SessionID[Inc] = '\0';
+	
+	fclose(TempDescriptor);
+	
+	/*We get everything from /proc.*/
+	ProcDir = opendir("/proc/");
+	
+	while ((CurDir = readdir(ProcDir)))
+	{
+		if (isdigit(CurDir->d_name[0]) && CurDir->d_type == 4)
+		{			
+			CurPID = atoi(CurDir->d_name); /*Convert the new PID to a true number.*/
+			
+			if (CurPID == 1 || CurPID == OurPID)
+			{ /*Don't try to kill init, or us.*/
+				continue;
+			}
+			
+			snprintf(TmpBuf, 1024, "/proc/%lu/sessionid", CurPID);
+			
+			if (!(TempDescriptor = fopen(TmpBuf, "r")))
+			{
+				fprintf(stderr, "Failed to read session ID file for process %lu. Aborting.", CurPID);
+				return 1;
+			}
+			
+			/*Copy in the contents of the file sessionid, using EOF to know when to stop. NOW SHUT UP ABOUT THE LOOPS!
+			 * I can't really do this with fread() because these files report zero length!*/
+			for (Inc = 0; (TChar = getc(TempDescriptor)) != EOF; ++Inc)
+			{
+				SessionID_Targ[Inc] = TChar;
+			}
+			SessionID_Targ[Inc] = '\0';
+			
+			fclose(TempDescriptor);
+			
+			if (!strncmp(SessionID, SessionID_Targ, 8192))
+			{ /*It's in our session ID, so don't touch it.*/
+				continue;
+			}
+			
+			/*We made it this far, must be safe to nuke this process.*/
+			kill(CurPID, InSignal); /*Actually send the kill, stop, whatever signal.*/
+		}
+	}
+	closedir(ProcDir);
+	
+	return 0;
 }
